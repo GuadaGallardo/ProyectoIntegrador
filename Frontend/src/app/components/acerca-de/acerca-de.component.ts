52c429c04b7f3201f708d26a0129695185967f8d
GuadaGallardo <guadalupegallardo@hotmail.com.ar>
2023-05-14 16:57:59 -0300

    inicio_de_sesion

--- Frontend/src/app/components/acerca-de/acerca-de.component.ts
+++ Frontend/src/app/components/acerca-de/acerca-de.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { PersonaService } from 'src/app/service/persona.service';
 
 @Component({
   selector: 'app-acerca-de',
-  templateUrl: './acerca-de.component.html',
+  //templateUrl: './acerca-de.component.html',
   styleUrls: ['./acerca-de.component.css']
 })
-export class AcercaDeComponent {
+export class AcercaDeComponent {}
+ // Persona persona = new Persona("","","");
 
-}
+  //constructor(public personaService: PersonaService) { }
+
+  //suscribe es un metodo que escucha siempre que el observable haga un cambio
+  //ngOnInit(): void {
+    //this.personaService.encontrarPersona().subscribe(data => {this.Persona = data})
+  //
